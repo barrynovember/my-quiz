977788eb77583b50ac9b26670584185ec6a31ee7
barry <barrynovember1@gmail.com>
2018-04-23 13:40:21 +0200

    First commit

---
+++ quiz.js
@@ -0,0 +1,244 @@
+var myQuestions =
+{
+  1: {
+    'question': "<p>Question 1</p><p>How many steps would you have to take before you reach the first level of the Eiffel Tower?</p>",
+    'answers': {
+      'a': '3',
+      'b': '300',
+      'c': '115',
+      'd': '25'
+    },
+    'correctAnswer': 'b'
+  },
+  2: {
+    'question': "<p>Question 2</p><p>What is a group of ravens known as?</p>",
+    'answers': {
+      'a': 'A horde',
+      'b': 'An unkindness',
+      'c': 'A wake',
+      'd': 'A party'
+    },
+    'correctAnswer': 'b'
+  },
+  3: {
+    'question': "<p>Question 3</p><p>What is a group of crows called?</p>",
+    'answers': {
+      'a': 'A murder',
+      'b': 'A swarm',
+      'c': 'A seige',
+      'd': 'A scold'
+    },
+    'correctAnswer': 'a'
+  },
+  4: {
+    'question': "<p>Question 4</p><p>What is a group of unicorns known as?</p>",
+    'answers': {
+      'a': 'A pod',
+      'b': 'A platoon',
+      'c': 'A blessing',
+      'd': 'A herd'
+    },
+    'correctAnswer': 'c'
+  },
+  5: {
+    'question': "<p>Question 5</p><p>How old is the world's oldest piece of chewing gum?</p>",
+    'answers': {
+      'a': '805yrs',
+      'b': '22yrs',
+      'c': '7yrs',
+      'd': '9000 yrs'
+    },
+    'correctAnswer': 'd'
+  },
+  6: {
+    'question': "<p>Question 6</p><p>Which animal is known to kill more people than plane crashes</p>",
+    'answers': {
+      'a': 'Donkeys',
+      'b': 'Shark',
+      'c': 'Dog',
+      'd': 'Monkey'
+    },
+    'correctAnswer': 'a'
+  },
+  7: {
+    'question': "<p>Question 7</p><p>The Empire State Building is composed of how many bricks?</p>",
+    'answers': {
+      'a': '100 thousand',
+      'b': '10 thousand',
+      'c': '10 million',
+      'd': '3 million'
+    },
+    'correctAnswer': 'c'
+  },
+  8: {
+    'question': "<p>Question 8</p><p>How many eggs does a female mackerel lay at a time?</p>",
+    'answers': {
+    'a': '500,000',
+    'b': '5',
+    'c': '10',
+    'd': '1000'
+    },
+    'correctAnswer': 'a'
+  },
+  9: {
+    'question': "<p>Question 9</p><p>What is the Karni Mata Temple, Deshnoke, India, famous for?</p>",
+    'answers': {
+    'a': 'Its 5,000 monkeys.',
+    'b': 'Its 10,000 vampire bats.',
+    'c': 'Its 2,000 venomous snakes.',
+    'd': 'Its 20,000 residents of black rats.'
+    },
+    'correctAnswer': 'd'
+  },
+  10: {
+    'question': "<p>Question 10</p><p>How long is a kangaroo baby, when it is born?</p>",
+    'answers': {
+    'a': '30cm',
+    'b': '25mm',
+    'c': '10cm',
+    'd': '0.5m'
+    },
+    'correctAnswer': 'b'
+  },
+  11: {
+    'question': "<p>Question 11</p><p>Which animal swallows large stones and uses them as ballast?</p>",
+    'answers': {
+      'a': 'Hippo',
+      'b': 'Turtle',
+      'c': 'Crocodile.',
+      'd': 'Seal'
+    },
+    'correctAnswer': 'c'
+  },
+  12: {
+    'question': '<p>Question 12</p><p>What is the dot over the letter "i" and "j" called?</p>',
+    'answers': {
+      'a': 'A tittle',
+      'b': 'A point',
+      'c': 'A nipple',
+      'd': 'A tick'
+    },
+    'correctAnswer': 'a'
+  },
+  13: {
+    'question': "<p>Question 13</p><p>What is the world's most popular first name?</p>",
+    'answers': {
+      'a': 'John',
+      'b': 'Muhammad',
+      'c': 'Yi',
+      'd': 'Juan'
+    },
+    'correctAnswer': 'b'
+  },
+  14: {
+    'question': "<p>Question 14</p><p>How much time, on an average, does a person spend on kissing, in his lifetime?</p>",
+    'answers': {
+      'a': '20,160 minutes!',
+      'b': '5,000 minutes!',
+      'c': '60,600 minutes!',
+      'd': '4,800 minutes'
+    },
+    'correctAnswer': 'a'
+  },
+  15: {
+    'question': "<p>Question 15</p><p>How much time, on an average, does a person spend in the toilet in his lifetime?</p>",
+    'answers': {
+      'a': '2yrs.',
+      'b': '1yr.',
+      'c': '6yrs.',
+      'd': '3yrs.'
+    },
+    'correctAnswer': 'd'
+  },
+  16: {
+    'question': "<p>Question 16</p><p>When a burning cigarette is inhaled, what is the temperature at its tip?</p>",
+    'answers': {
+      'a': '90 degree Celsius!',
+      'b': '120 degree Celsius!',
+      'c': '300 degree Celsius!',
+      'd': '700 degree Celsius!'
+    },
+    'correctAnswer': 'd'
+  },
+  17: {
+    'question': "<p>Question 17</p><p>What does the reverse of the Nobel Peace Prize depict?</p>",
+    'answers': {
+      'a': 'The Four Horsemen of the Apocalypse.',
+      'b': 'Men in the embrace of angles.',
+      'c': 'Three naked men, who are embracing one another.',
+      'd': 'Three doves in flight.'
+    },
+    'correctAnswer': 'c'
+  },
+  18: {
+    'question': "<p>Question 18</p><p>How many pairs of underwear do men buy in a year?</p>",
+    'answers': {
+      'a': 'An average of 5!',
+      'b': 'An average of 3.4!',
+      'c': 'An average of 6.5!',
+      'd': 'An average of 8!'
+    },
+    'correctAnswer': 'b'
+  },
+  19: {
+    'question': "<p>Question 19</p><p>How big does the pupil of the eye become, when it looks at something pleasing?</p>",
+    'answers': {
+      'a': 'It expands by as much as 45 percent of its normal size.',
+      'b': 'It expands by as much as 10 percent of its normal size.',
+      'c': 'It expands by as much as 25 percent of its normal size.',
+      'd': 'It expands by as much as 30 percent of its normal size.'
+    },
+    'correctAnswer': 'a'
+  },
+  20: {
+    'question': "<p>Question 20</p><p>What ailment kills the most fruit flies?</p>",
+    'answers': {
+      'a': 'Age',
+      'b': 'Damaged wings',
+      'c': 'Constipation',
+      'd':  'Heart failure'
+    },
+    'correctAnswer': 'c'
+  }
+
+};
+
+var globalCnt = 0;//this is a global variable (ie. its accessible throughout the whole document) that counts the number of questions.
+var quiz ={//this is a class because it contains functions.
+  'initialize' : function(){//this is the function (initialize) we are calling when he HTML page loads
+    $.each(myQuestions, function(i,v){
+      globalCnt++;//increments by 1 the global variable count.
+      var myQuestion = myQuestions[i]['question'];
+      var _formElements = '';
+      $.each(myQuestions[i]['answers'], function(si,sv){
+        _formElements += '<div class="form-check">';//this is all outputs.
+          _formElements += si + '&nbsp;<input class="radioButtons " data-correctAnswer="' + myQuestions[i]['correctAnswer'] + '" type="radio" name="' + i + '" value="' + si + '"/>';
+          _formElements += '<label>';
+            _formElements += myQuestions[i]['answers'][si];
+          _formElements += '</label>';
+        _formElements += '</div>';
+      });
+
+      $('#quiz_form').append('<p>' + myQuestion + '</p><p>' + _formElements + '</p>');
+    });
+    var _formButton = '<a class="btn btn-primary" id="submitButton" href="javascript:quiz.submit();">Submit</a>';
+    $('#quiz_form').append(_formButton);
+  },//end of initialize function
+  'submit' : function(){
+    $('#submitButton').remove();//this where you remove the submitButton once its clicked. The parentheses is used as a get element on submitButton.
+    var correctCnt = 0; //declare correct count variable and set it to zero
+    $('.radioButtons').each(function(i,v){ //loop through each radio button with a class name of radioButtons
+      var theirAnswer = '';
+      var correctAnswer = '';
+      if ($(this).is(':checked')){ //check if the radio button is checked/clicked
+        theirAnswer = ($(this).val());//if the radio button is checked then assign that radio button's value to variable
+        correctAnswer = $(this).attr('data-correctAnswer');//check what the correct value is for the radio button
+        if (correctAnswer == theirAnswer){//compare the radio button value checked to the correct answer. if it equals each other then increment the correctCnt by 1.
+          correctCnt++;
+        }
+      }
+    });
+
+    $('#quiz_form').append('<p>Congratulations, you got ' + correctCnt + '/' + globalCnt + '</p>s');
+  }
+};
